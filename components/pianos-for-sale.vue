6b90435745d38ee689374392a9d1d4297f52ec08
Duane McGuire <biz@duanemcguire.com>
2020-10-16 16:02:12 -0600

    restored previously trashed content

--- components/pianos-for-sale.vue
+++ components/pianos-for-sale.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h4>Piano For Sale</h4>
+    <h4>Pianos For Sale</h4>
+    <div style="float:left; margin-right:5px; margin-bottom: 10px; border: 2px solid black; padding: 2px">
+      <a href="/blog/steinway-model-a-3-for-sale/">
+        <figure class="wp-block-image size-medium">
+          <img loading="lazy" width="300" src="/images/2017-03-03-15.28.02-300x225.jpg" alt="Steinway Model A3 (1922)">
+          <figcaption>Steinway Model A3 (1922)</figcaption>
+        </figure>
+      </a>
+    </div>
     <div style="float:left; margin-right:5px;  border: 2px solid black; padding: 2px">
       <a href="/blog/kawai-howard-piano-for-sale/">
         <figure class="wp-block-image size-medium">
