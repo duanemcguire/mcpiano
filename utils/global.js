a4da4df0afea1043f85198ab65caab76fee6c9ad
Duane McGuire <biz@duanemcguire.com>
2020-10-17 17:43:45 -0600

    sitemap

--- utils/global.js
+++ utils/global.js
@@ -8,8 +8,7 @@ export default {
   siteName: 'McGuire Piano',
   author: 'Duane McGuire',
   siteTitle: 'Piano Sales and Restoration by Duane McGuire, RPT',
-  siteDesc:
-    'McGuire Piano specializes in the restoration and repair of pianos in Utah. We also offer a select number of pianos for sale which have been restored or reconditioned in our shop',
+  siteDesc: 'McGuire Piano specializes in the restoration and repair of pianos in Utah. We also offer a select number of pianos for sale which have been restored or reconditioned in our shop',
   mainImage: 'https://mcguirepiano.com/images/medium/2017-03-03-15.28.02-1024x768.jpg',
   siteType: 'website',
 };
