f93fa592980e2194b94eda63b1a4d7dacb8bf341
Duane McGuire <biz@duanemcguire.com>
2020-10-17 08:47:52 -0600

    Rework top nav for mobile

--- components/page-header.vue
+++ components/page-header.vue
@@ -1,28 +1,15 @@
 <template>
 <div style="background-color: black" class="container d-md-flex align-items-stretch">
-  <div style="color: white" class="p-4 p-md-2 pt-5">
+  <div style="color: white" class="p-2 p-md-2 pt-2">
     <h2><a style="color: white;" href="/">McGuire Piano</a></h2>
     Piano Sales and Restoration by <BR />Duane McGuire, RPT
     <div></div>
   </div>
-  <div class="topnav p-4 p-md-2 pt-5" id="myTopnav">
+  <div class="topnav pt-3" id="myTopnav">
       <a href="/">Home</a>
       <a href="/blog">Blog</a>
       <a href="/category/pianos-for-sale">Pianos For Sale</a>
       <a href="/contact">Contact</a>
-      <a href="javascript:void(0);" class="icon" onclick="
-        javascript:navFunc();
-        function navFunc() {
-          var x = document.getElementById('myTopnav');
-          if (x.className === 'topnav') {
-            x.className += ' responsive';
-          } else {
-            x.className = 'topnav';
-          }
-      }" >
-        <i class="fa fa-bars"></i>
-      </a>
-
     </ul>
   </div>
 </div>
@@ -41,38 +28,9 @@
   background-color: #3d3d3d
 }
 
-.topnav {
-  overflow: hidden;
-}
-
-.topnav .icon {
-  display: none;
-}
-
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {display: none;}
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-}
 
 
-@media screen and (max-width: 600px) {
-  .topnav.responsive {
-    position: relative;
-  }
 
-  .topnav.responsive .icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
 
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-}
+
 </style>
