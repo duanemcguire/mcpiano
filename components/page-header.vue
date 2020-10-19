3a547bb7090892df14c24d1834dc3fa341b199ee
Duane McGuire <biz@duanemcguire.com>
2020-10-18 19:42:12 -0600

    erin-logo one

--- components/page-header.vue
+++ components/page-header.vue
@@ -1,36 +1,32 @@
 <template>
-<div style="background-color: black" class="container d-md-flex align-items-stretch pb-2">
-  <div style="color: white" class="p-2 p-md-2 pt-2 ">
-    <h2><a style="color: white;" href="/">McGuire Piano</a></h2>
-    Piano Sales and Restoration by <BR />Duane McGuire, RPT
-    <div></div>
+<div style="background-color: black;" class="container row" >
+  <div style="color: white" class="p-2">
+    <img src="/images/header-logo.png" height="70"><BR />
+  <div  class="p-2">
+    <i>Piano Sales and Restoration by <BR />Duane McGuire, RPT</i>
   </div>
-  <div class="topnav pt-3" id="myTopnav">
-      <a href="/">Home</a>
-      <a href="/blog">Blog</a>
-      <a href="/category/pianos-for-sale">Pianos For Sale</a>
-      <a href="/contact">Contact</a>
-    </ul>
+  <div class="topnav p-2" id="myTopnav">
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/category/pianos-for-sale">Pianos For Sale</a>
+    <a href="/contact">Contact</a>
   </div>
+  <div  class="p-2">
+    <img height="125" src="/images/header-piano.png">
+  </div>
+</div>
 </div>
 </template>
 <style>
-
 .topnav a {
   line-height: 1.2;
   font-size: 0.8em;
   padding: 3px;
   text-transform: uppercase;
   color: white;
-  border: 1px solid gray;
   display: inline-block;
+  border: 1px solid gray;
   margin-right: 5px;
   background-color: #3d3d3d
 }
-
-
-
-
-
-
 </style>
