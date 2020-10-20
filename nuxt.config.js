1166037f685ed3a35cdcb0e93276b85a435caf87
Duane McGuire <biz@duanemcguire.com>
2020-10-19 20:18:49 -0600

    Google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,8 @@ export default {
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '@/plugins/components',
-    '@/plugins/lightbox.client'
+    '@/plugins/lightbox.client',
+    { src: '~plugins/ga.js', mode: 'client' }
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
